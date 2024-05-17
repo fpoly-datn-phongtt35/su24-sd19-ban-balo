4e60872735277bed2ad29618327e79c5f5aacd5a
NGuyen Van Thanh <tv24603@gmail.com>
2024-05-17 21:24:35 +0700

    first commit

---
+++ src/main/java/com/example/datntest/DatNtestApplication.java
@@ -0,0 +1,13 @@
+package com.example.datntest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatNtestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatNtestApplication.class, args);
+    }
+
+}
